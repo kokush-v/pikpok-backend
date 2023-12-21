9e0fe6a1807a292eaf1abb92a0ad135ecf046fe3
kokush <witalik.logoyda@gmail.com>
2023-12-21 21:34:59 +0200

    Refactor socket event handlers

--- src/socket.ts
+++ src/socket.ts
@@ -14,29 +14,33 @@ const io = new Server(httpServer, {
 });
 
 io.on("connection", async (socket) => {
-	socket.on("changeRoom", async (room) => {
-		for (const room of socket.rooms) {
-			if (room !== socket.id) socket.leave(room);
-		}
+	try {
+		socket.on("changeRoom", async (room) => {
+			for (const room of socket.rooms) {
+				if (room !== socket.id) socket.leave(room);
+			}
 
-		socket.join(room);
-	});
+			socket.join(room);
+		});
 
-	socket.on("message", async (message: Message) => {
-		const room = Array.from(socket.rooms).filter((room) => room !== socket.id)[0];
-		const dbMesasge = await saveMessage(room, message);
-		const user = await findUserById(message.userId);
+		socket.on("message", async (message: Message) => {
+			const room = Array.from(socket.rooms).filter((room) => room !== socket.id)[0];
+			const dbMesasge = await saveMessage(room, message);
+			const user = await findUserById(message.userId);
 
-		if (!user || !dbMesasge) throw Error("Bad request");
+			if (!user || !dbMesasge) throw Error("Bad request");
 
-		const msg = {
-			user: { name: user.username, avatar: user.avatarUrl },
-			id: dbMesasge.messageId,
-			text: dbMesasge.text,
-		};
+			const msg = {
+				user: { name: user.username, avatar: user.avatarUrl },
+				id: dbMesasge.messageId,
+				text: dbMesasge.text,
+			};
 
-		socket.emit("message", msg);
-	});
+			socket.emit("message", msg);
+		});
 
-	socket.on("disconnect", () => {});
+		socket.on("disconnect", () => {});
+	} catch (error) {
+		console.log(error);
+	}
 });
